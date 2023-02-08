f5e703b5a447bfef0d8ee78ecab942bd006be963
mehmetakif.tatar <mehmetakif.tatar@birlesikodeme.com>
2023-02-08 23:44:53 +0300

    feat: improve query

--- internal/infrastructure/persistent/Query.go
+++ internal/infrastructure/persistent/Query.go
@@ -2,36 +2,30 @@ package persistent
 
 import (
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func QueryData(dq *DataQuery) []primitive.M {
-	datas := []bson.M{
-		{
-			"$match": bson.M{
-				"createdAt": bson.M{
-					"$gt": dq.StartDate,
-					"$lt": dq.EndDate,
-				},
-			},
-		},
-		{
-			"$project": bson.M{
-				"_id":        0,
-				"key":        1,
-				"createdAt":  1,
-				"totalCount": bson.M{"$sum": "$counts"},
-			},
-		},
-		{
-			"$match": bson.M{
-				"totalCount": bson.M{
-					"$gt": dq.MinCount,
-					"$lt": dq.MaxCount,
-				},
-			},
-		},
-	}
+func QueryData(dq *DataQuery) mongo.Pipeline {
+	filterCreateDate := bson.D{{"$match", bson.D{
+		{"createdAt", bson.D{
+			{"$gte", dq.StartDate},
+			{"$lt", dq.EndDate},
+		}}}}}
 
-	return datas
+	project := bson.D{{"$project", bson.D{
+		{"_id", false},
+		{"key", true},
+		{"createdAt", true},
+		{"totalCount", bson.D{
+			{"$sum", "$counts"},
+		}}}}}
+
+	filterTotalCount := bson.D{{"$match", bson.D{
+		{"totalCount", bson.D{
+			{"$gte", dq.MinCount},
+			{"$lt", dq.MaxCount},
+		}},
+	}}}
+
+	return mongo.Pipeline{filterCreateDate, project, filterTotalCount}
 }
